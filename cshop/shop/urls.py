8b7e06c318af269c8a0a7f465d85a1d51569e52b
Alm4my <thenixadm@gmail.com>
2021-10-24 07:51:54 +0000

    Add constant for media and configure devel server to serve static files

--- cshop/shop/urls.py
+++ cshop/shop/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from shop.views import ProductList, ProductDetail
@@ -9,3 +11,6 @@ urlpatterns = [
     path('<slug:category_slug>/', ProductList.as_view(), name='product_list_by_category'),
     path('<int:pk>/<slug:slug>/', ProductDetail.as_view(), name='product_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
