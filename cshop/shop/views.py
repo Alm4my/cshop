d6bd6f5f1d49c1578957fbb5513c6d8c8b140455
Alm4my <thenixadm@gmail.com>
2021-10-24 07:25:29 +0000

    Create view for ProductDetail

--- cshop/shop/views.py
+++ cshop/shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from shop.models import Category, Product
 
@@ -7,18 +7,28 @@ from shop.models import Category, Product
 class ProductList(ListView):
     model = Product
     template_name = 'shop/product/list.html'
-    context_object_name = 'product_list'  # name of the list as a template variable default to model_list or object_list
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['category_list'] = Category.objects.all()
-        context['product_list'] = Product.objects.filter(available=True)
         slug = self.kwargs.get('category_slug')
-
         if slug:
             context['category'] = get_object_or_404(Category, slug=slug)
             context['product_list'] = Product.objects.filter(
                 category=context['category']
             )
-
+            return context
+        # no slug (base case)
+        context['category_list'] = Category.objects.all()
+        context['product_list'] = Product.objects.filter(available=True)
         return context
+
+
+class ProductDetail(DetailView):
+    template_name = 'shop/product/detail.html'
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        return context
+
