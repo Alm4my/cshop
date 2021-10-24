ef2bf2a900a6cc08870211b1485f08a6b9f2e450
Alm4my <thenixadm@gmail.com>
2021-10-24 07:24:54 +0000

    Add get_absolute_url to model

--- cshop/shop/models.py
+++ cshop/shop/models.py
@@ -1,5 +1,6 @@
 from django.db.models import Model, CharField, SlugField, ForeignKey, CASCADE, ImageField, TextField, DecimalField, \
     BooleanField, DateTimeField
+from django.urls import reverse
 
 
 class Category(Model):
@@ -32,3 +33,6 @@ class Product(Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
