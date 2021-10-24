166561849e8a0d72733e8591eab03ceee661bfb5
Alm4my <thenixadm@gmail.com>
2021-10-24 05:04:47 +0000

    Add created models to admin

--- cshop/shop/admin.py
+++ cshop/shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from shop.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
