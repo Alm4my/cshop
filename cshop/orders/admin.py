65c606a909c3d68de810c76c6b9ad99ec126b81d
Alm4my <thenixadm@gmail.com>
2021-10-25 13:29:55 +0000

    Comment out raw_id_fields

--- cshop/orders/admin.py
+++ cshop/orders/admin.py
@@ -6,7 +6,7 @@ from orders.models import Order, OrderItem
 # Register your models here.
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
-    raw_id_fields = ['product']
+    # raw_id_fields = ['product']
 
 
 @admin.register(Order)
