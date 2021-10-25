c0de19578c8a56ae92bc4a48eab6a304f508ea85
Alm4my <thenixadm@gmail.com>
2021-10-25 14:07:17 +0000

    Create views for ordering items

--- cshop/orders/views.py
+++ cshop/orders/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import View
+
+from cart.cart import Cart
+from orders.forms import OrderCreateForm
+from orders.models import OrderItem
+
+
+class OrderCreate(View):
+    get_template_name = 'orders/order/create.html'
+    post_template_name = 'orders/order/created.html'
+
+    def post(self, request, *args, **kwargs):
+        cart = Cart(request)
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            # clear the cart
+            cart.clear()
+            return render(request, self.post_template_name, {'order': order})
+        return self.get(request)
+
+    def get(self, request):
+        return render(request, self.get_template_name, {'form': OrderCreateForm()})
