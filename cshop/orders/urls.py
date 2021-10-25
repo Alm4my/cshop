6755c0c62fadd39a7f17a9c4dfe307493caad28e
Alm4my <thenixadm@gmail.com>
2021-10-25 20:50:07 +0000

    Add urls for pdf generation

--- cshop/orders/urls.py
+++ cshop/orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from orders.views import OrderCreate, AdminOrderDetail
+from orders.views import OrderCreate, AdminOrderDetail, AdminOrderPdf
 
 app_name = 'orders'
 
 urlpatterns = [
     path('create/', OrderCreate.as_view(), name='order_create'),
     path('admin/order/<int:pk>', AdminOrderDetail.as_view(), name='admin_order_detail'),
+    path('admin/order/<int:pk>/pdf', AdminOrderPdf.as_view(), name='admin_order_pdf'),
 ]
