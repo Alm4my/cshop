dfd5728d76d185d316acb5d1d93053bb3b3b2482
Alm4my <thenixadm@gmail.com>
2021-10-27 17:49:09 +0000

    Add translation for the different urls

--- cshop/orders/urls.py
+++ cshop/orders/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
 
 from orders.views import OrderCreate, AdminOrderDetail, AdminOrderPdf
 
 app_name = 'orders'
 
 urlpatterns = [
-    path('create/', OrderCreate.as_view(), name='order_create'),
+    path(_('create/'), OrderCreate.as_view(), name='order_create'),
     path('admin/order/<int:pk>', AdminOrderDetail.as_view(), name='admin_order_detail'),
     path('admin/order/<int:pk>/pdf', AdminOrderPdf.as_view(), name='admin_order_pdf'),
 ]
--- cshop/payment/urls.py
+++ cshop/payment/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-
+from django.utils.translation import gettext_lazy as _
 from payment.views import PaymentProcess, PaymentDone, PaymentCancelled
 
 app_name = 'payment'
 
 urlpatterns = [
-    path('process/', PaymentProcess.as_view(), name='process'),
-    path('done/', PaymentDone.as_view(), name='done'),
-    path('cancelled/', PaymentCancelled.as_view(), name='cancelled'),
+    path(_('process/'), PaymentProcess.as_view(), name='process'),
+    path(_('done/'), PaymentDone.as_view(), name='done'),
+    path(_('cancelled/'), PaymentCancelled.as_view(), name='cancelled'),
 ]
