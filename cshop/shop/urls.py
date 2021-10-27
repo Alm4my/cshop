45e92b1f369838d7ac29690b2ce79e37cd2feb3f
Alm4my <thenixadm@gmail.com>
2021-10-27 07:11:09 +0000

    Add url for coupons and update other urls

--- cshop/shop/urls.py
+++ cshop/shop/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from shop.views import ProductList, ProductDetail
@@ -11,4 +9,3 @@ urlpatterns = [
     path('<slug:category_slug>/', ProductList.as_view(), name='product_list_by_category'),
     path('<int:pk>/<slug:slug>/', ProductDetail.as_view(), name='product_detail'),
 ]
-
