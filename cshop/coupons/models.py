a13d1a0501f3ba4f768ca2494037cc551767a93a
Alm4my <thenixadm@gmail.com>
2021-10-27 06:29:17 +0000

    Add coupon model and make migration

--- cshop/coupons/models.py
+++ cshop/coupons/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models import Model, CharField, DateTimeField, IntegerField, BooleanField
 
-# Create your models here.
+
+class Coupon(Model):
+    code = CharField(max_length=50, unique=True)
+    valid_from = DateTimeField()
+    valid_to = DateTimeField()
+    discount = IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    active = BooleanField()
+
+    def __str__(self):
+        return self.code
