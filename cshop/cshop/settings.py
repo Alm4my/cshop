73b4af5046e40fc37553ffee65246e4146710849
Alm4my <thenixadm@gmail.com>
2021-10-27 08:56:28 +0000

    Add language code in the settings of the application and add the local middleware
    Note: the middleware needs to be placed after the session middleware since it uses session data and before the common middleware because the latter needs an active language to resolve the requested URL.

--- cshop/cshop/settings.py
+++ cshop/cshop/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -112,7 +113,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
+LANGUAGES = (
+    ('en', 'English'),
+    ('fr', 'French'),
+)
 
 TIME_ZONE = 'UTC'
 
